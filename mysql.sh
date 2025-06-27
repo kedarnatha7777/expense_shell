2286df218df747599b58bd33d8e1096b2e724e4c
kedarnatha7777 <kedarnath.a7777@gmail.com>
2025-06-28 02:29:42 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -42,7 +42,7 @@ systemctl start mysqld &>>$LOG_FILE
 VALIDATION_FUNCTION $? "starting  mysqld"
 
 mysql -h db.78skedar.online -uroot -p${db_password} -e 'SHOW DATABASES' &>>$LOG_FILE
-if [ $? -eq 0 ]
+if [ $? -nq 0 ]
     then
         echo "mysql root password is already setuped "
     else

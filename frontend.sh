af5530345f271c21a3d4a71d1cb232b86cd069e1
kedarnatha7777 <kedarnath.a7777@gmail.com>
2025-06-28 19:06:29 +0530

    expense -frontend

--- frontend.sh
+++ frontend.sh
@@ -30,23 +30,23 @@ VALIDATION_FUNCTION(){
     fi 
 }
 
-dnf install nginx -y 
+dnf install nginx -y &>>$LOG_FILE
 VALIDATION_FUNCTION $? "install nginx "
 
-systemctl enable nginx
+systemctl enable nginx &>>$LOG_FILE
 VALIDATION_FUNCTION $? "enabling  nginx "
-systemctl start nginx
+systemctl start nginx &>>$LOG_FILE
 VALIDATION_FUNCTION $? "starting  nginx "
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
 VALIDATION_FUNCTION $? "removing the default content  "
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
-VALIDATION_FUNCTION $? "downode the frontend code "
-cd /usr/share/nginx/html/
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATION_FUNCTION $? "downlode the frontend code "
 
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html/
+unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATION_FUNCTION $? "install nginx "
-cp /home/ec2-user/expense_shell/frontend.service /etc/nginx/default.d/expense.conf 
+cp /home/ec2-user/expense_shell/frontend.service /etc/nginx/default.d/expense.conf &>>$LOG_FILE
 VALIDATION_FUNCTION $? "copying the expense.conf "
-systemctl restart nginx
+systemctl restart nginx &>>$LOG_FILE
 VALIDATION_FUNCTION $? "restart nginx "
